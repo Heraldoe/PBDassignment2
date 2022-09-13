511b46feddb1134ddbeb6799df0d25bb346a318b
Halomoan Geraldo <111977201+Heraldoe@users.noreply.github.com>
2022-09-13 21:45:28 +0700

    views.py was changed

--- katalog/views.py
+++ katalog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
 def show_catalogue(request):
-    return render(request, "katalog.html")
+    cataloguedata = CatalogItem.objects.all()
+    ddata = {'items' : cataloguedata,
+     "Name" : "Halomoan Geraldo",
+      "NPM" : "2106720885"}
+    return render(request, "katalog.html", ddata)
