4d8496868e45ece4f2327d0d5ec4c2114a408426
Halomoan Geraldo <111977201+Heraldoe@users.noreply.github.com>
2022-09-21 19:54:52 +0700

    essay and tests

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import resolve
 
 # Create your tests here.
+class test(TestCase):
+    def testhtml(self):
+        response = Client().get('/mywatchlist/html/')
+        self.assertEqual(response.status_code,200)
+    
+    def testxml(self):
+        response = Client().get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
+    
+    def testjson(self):
+        response = Client().get('/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
