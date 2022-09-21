57523ace0fd9cd0d273260e950278c06e755516b
Halomoan Geraldo <111977201+Heraldoe@users.noreply.github.com>
2022-09-21 12:37:57 +0700

    Almost done

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class ItemWatchlist(models.Model):
     watched = models.TextField()
     title = models.TextField()
-    rating = models.IntegerField()
+    rating = models.TextField()
     release_date = models.CharField(max_length=255)
     review = models.TextField()
