49fd90dc353a693d38d877259801e32c940febd2
Halomoan Geraldo <111977201+Heraldoe@users.noreply.github.com>
2022-09-28 21:54:09 +0700

    assignment 4 done?

--- todolist/models.py
+++ todolist/models.py
@@ -1,8 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Task(models.Model):
-    user = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
     date = models.DateField()
     title = models.TextField()
     description = models.TextField()
--- todolist/views.py
+++ todolist/views.py
@@ -1,5 +1,68 @@
 from django.shortcuts import render
+from todolist.models import Task
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+@login_required(login_url='/todolist/login/')
 def show_todolist(request):
-    return render(request, "todolist.html")
+    todolistdata = Task.objects.filter(user = request.user)
+    moredata = { 'todolist' : todolistdata,
+                'username' : request.user,
+    "last_login" : request.COOKIES['last_login']}
+    return render(request, "todolist.html", moredata)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account successfully created!')
+            return redirect('todolist:login')
+
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("todolist:show_todolist"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Wrong Username or Password!')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('todolist:login'))
+    response.delete_cookie('last_login')
+    return response
+
+@login_required(login_url='/todolist/login/')
+def create_task(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        Task.objects.create(
+            user=request.user,
+            title=title,
+            description=description,
+            date=datetime.datetime.today(),
+        )
+        return HttpResponseRedirect(reverse("todolist:show_todolist"))
+    return render(request, "create_task.html")
